f584de6b4d8e96fb8440cfbcd4409ee57e75064a
rckmnz <rckmnz05@gmail.com>
2017-08-14 18:12:43 -0400

    Made api contract

--- database/tables.sql
+++ database/tables.sql
@@ -4,14 +4,14 @@ CREATE DATABASE middlewhere;
 
 USE middlewhere;
 
-CREATE TABLE `org` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) NOT NULL DEFAULT '',
-  `payment` varchar(100) DEFAULT NULL,
-  `code` varchar(100) DEFAULT NULL,
-  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+-- CREATE TABLE `org` (
+--   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+--   `name` varchar(60) NOT NULL DEFAULT '',
+--   `payment` varchar(100) DEFAULT NULL,
+--   `code` varchar(100) DEFAULT NULL,
+--   `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--   PRIMARY KEY (`id`)
 
 
 
@@ -21,12 +21,17 @@ CREATE TABLE `org` (
 
 CREATE TABLE `projects` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL DEFAULT '',
+  `title` varchar(100) NOT NULL DEFAULT '',
   `description` varchar(140) DEFAULT NULL,
+  `adminUserId` int(11) NOT NULL, --foreign key
   `deadline` datetime DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+
+  --make sql queries necesseries to generate progress
+  ---- retrieve a list of all projects along with a % completion
+  ---- retrieve a list of all projects where a certain userId has some tasks assigned
 
 
 
@@ -48,18 +53,18 @@ CREATE TABLE `sessions` (
 # Dump of table subtasks
 # ------------------------------------------------------------
 
-CREATE TABLE `subtasks` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL DEFAULT '',
-  `milestones` varchar(100) NOT NULL DEFAULT '',
-  `completed` tinyint(1) NOT NULL,
-  `description` varchar(140) DEFAULT NULL,
-  `deadline` datetime DEFAULT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `priority` tinyint(1) DEFAULT NULL,
-  `milestoneId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+-- CREATE TABLE `subtasks` (
+--   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+--   `name` varchar(100) NOT NULL DEFAULT '',
+--   `milestones` varchar(100) NOT NULL DEFAULT '',
+--   `completed` tinyint(1) NOT NULL,
+--   `description` varchar(140) DEFAULT NULL,
+--   `deadline` datetime DEFAULT NULL,
+--   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--   `priority` tinyint(1) DEFAULT NULL,
+--   `milestoneId` int(11) DEFAULT NULL,
+--   PRIMARY KEY (`id`)
 
 
 
@@ -69,14 +74,14 @@ CREATE TABLE `subtasks` (
 
 CREATE TABLE `tasks` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL DEFAULT '',
-  `completed` tinyint(1) NOT NULL,
+  `title` varchar(100) NOT NULL DEFAULT '',
   `description` varchar(140) DEFAULT NULL,
   `deadline` datetime DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `priority` tinyint(1) DEFAULT NULL,
-  `taskId` int(11) DEFAULT NULL,
+  `priority` enum('low', 'normal', 'high') DEFAULT 'normal',
+  `projectId` int(11) NOT NULL, -- add foreign key
+  `completed` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 
 
@@ -89,10 +94,8 @@ CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL DEFAULT '',
   `password` varchar(60) NOT NULL DEFAULT '',
-  `org` varchar(60) NOT NULL DEFAULT '',
-  `special` tinyint(1) NOT NULL,
   `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updtedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 
 
@@ -102,7 +105,11 @@ CREATE TABLE `users` (
 # ------------------------------------------------------------
 
 CREATE TABLE `usersForTask` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `userId` int(11) DEFAULT NULL,
   `taskId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`,`taskId`) -- foreign keys for user and tasks
+
+
+
